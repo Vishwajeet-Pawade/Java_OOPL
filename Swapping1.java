383ef5083bc113edc1242d39c61f5f926d641145
Vishwajeet-Pawade <vishwajeetpawade7@gmail.com>
2025-07-31 10:50:10 +0530

    Add files via upload

---
+++ Swapping1.java
@@ -0,0 +1,14 @@
+public class Swapping1{
+public static void main(String args[]){
+int a=20;
+int b=10;
+int temp;
+System.out.printf("Value of a and b before swapping is:%d and %d\n",a,b);
+
+temp=a;
+a=b;
+b=temp; 
+System.out.printf("Value of a and b After swapping is:%d and %d",a,b);
+
+}
+}
